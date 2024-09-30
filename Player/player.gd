344d31e97c4ab343445d7d46f63b5a0d3f25e238
Charles Davenport <cdavenport090@gmail.com>
2024-09-29 21:20:02 -0400

    player movement

--- Player/player.gd
+++ Player/player.gd
@@ -8,7 +8,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	var move_input := Input.get_vector("left", "right", "up", "down")
+	velocity = move_input * 500.0
+	move_and_slide()
 
 func _die() -> void:
 	print("player died!")
